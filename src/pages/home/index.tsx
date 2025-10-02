fb10e3af6c2d599452af15ab0bf97179d3ab06c2
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-10-02 16:54:13 -0300

    consuming data from the api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,63 @@
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { FormEvent } from "react";
+
+interface CoinProps {
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCaUsd: string;
+  volumeUsd24Hr: string;
+  explorer: string;
+}
+
+interface dataProp {
+  data: CoinProps[];
+}
 
 function Home() {
   const [input, setInput] = useState("");
+  const [coins, setcoins] = useState<CoinProps[]>([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    fetch("https://rest.coincap.io/v3/assets?limit=10&offset=0", {
+      headers: {
+        Authorization:
+          "Bearer 5649947b97ae6e773ac165d316c95ace1ff39dabdadd2f3d0ef602e66ca8a0f1",
+      },
+    })
+      .then((response) => response.json())
+      .then((data: dataProp) => {
+        const coinsData = data.data;
+
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USd",
+        });
+        const formatedResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.priceUsd)),
+          };
+          return formated;
+        });
+
+        console.log(formatedResult);
+      });
+  }
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
