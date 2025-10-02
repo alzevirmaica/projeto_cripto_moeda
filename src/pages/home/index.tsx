72c88f5bc9eb7ba3068ed9f7b293bf72c8fdb1ec
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-10-01 23:58:07 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./home.module.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>teste Home</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>Página 404, está página não existe!</h1>
+    </div>
+  );
+}
+
+export default NotFound;
