c59fe88f9c137cdb49ef783057cdecd8a0335914
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-10-03 12:09:19 -0300

    Finishing project

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import type { FormEvent } from "react";
 
-interface CoinProps {
+export interface CoinProps {
   id: string;
   name: string;
   symbol: string;
